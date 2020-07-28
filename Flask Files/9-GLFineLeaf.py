191e776684e5e0fb1998a1b66ee662a08bf30d7b
sidhant237 <67872575+sidhant237@users.noreply.github.com>
2020-07-28 12:51:27 +0530

    Incomplete
    Append 3 cursors, which each returns 3 rows (by division)

--- Flask Files/9-GLFineLeaf.py
+++ Flask Files/9-GLFineLeaf.py
@@ -20,57 +20,51 @@ mysql = MySQL(app)
 def displayteamade():
       cur = mysql.connection.cursor()
       cur1 = mysql.connection.cursor()
+      cur2 = mysql.connection.cursor()
       # d1 = "'" + (str(request.args.get("start"))) + "'"
-      # d2 = "'" + (str(request.args.get("end"))) + "'"
+      # d11 = "'" + (str(request.args.get("end"))) + "'"
       # grp = "'" + (str(request.args.get("grpby"))) + "'"
-      d1 = "'2020-07-01'"
+      d1 = "'2020-07-02'"
+      d11 = "'2019-07-02'"
+      rv = []
+      column_headers = ['1', '2', '3', '4']
 
-      #1
-      val = "FINEPERENTRY.FL_ID,DIVTAB2.DIV_NAME2, FINEPERENTRY.GL19_VAL, FINEPERENTRY.FL_PER "
-      tab = "FINEPERENTRY ,DIVTAB2"
-      joi = "FINEPERENTRY.DIV_ID2=DIVTAB2.DIV_ID2"
-      cur.execute(f'''select {val} from {tab} where {joi} and date = {d1}''')
-      rv = cur.fetchall()
+      #GL TODAY
+      val = "DIVTAB.DIV_NAME, sum(GL_VAL)"
+      tab = "FIELDENTRY, DIVTAB, SECTAB"
+      joi = "(FIELDENTRY.SEC_ID=SECTAB.SEC_ID) AND (SECTAB.DIV_ID = DIVTAB.DIV_ID)"
+      job = "FIELDENTRY.JOB_ID = 1"
+      cur.execute(f'''select {val} from {tab} where {joi} AND {job} and date = {d1} GROUP BY DIVTAB.DIV_NAME''')
+      rv.append (cur.fetchall()[0[0]])
 
-      #2
-      val = "sum(FINEPERENTRY.GL19_VAL)"
-      tab = "FINEPERENTRY ,DIVTAB2"
-      joi = "FINEPERENTRY.DIV_ID2=DIVTAB2.DIV_ID2"
-      cur1.execute(f'''select {val} from {tab} where {joi} group by FINEPERENTRY.DIV_ID2 ''')
-      rv1 = cur1.fetchall()
+      #GLTODAY LY
+      val1 = "sum(GL_VAL)"
+      tab1 = "FIELDENTRY, DIVTAB, SECTAB"
+      joi1 = "(FIELDENTRY.SEC_ID=SECTAB.SEC_ID) AND (SECTAB.DIV_ID = DIVTAB.DIV_ID)"
+      job1 = "FIELDENTRY.JOB_ID = 1"
+      cur1.execute(f'''select {val1} from {tab1} where {joi1} AND {job1} and date = {d11} GROUP BY DIVTAB.DIV_NAME''')
+      rv.append (cur1.fetchall()[0[0]])
 
-      #for row1 in rv1:
-      #       json_data.append(dict(zip(row_headers, row1)))
-      #return json.dumps(json_data)
+      # FINE LEAF% TODAYS GL
+      val2 = 'sum(FL_PER)'
+      tab2 = "FLENTRY, DIVTAB"
+      joi2 = "(FLENTRY.DIV_ID = DIVTAB.DIV_ID)"
+      cur2.execute(f'''select {val2} from {tab2} where {joi2} and date = {d1} GROUP BY DIVTAB.DIV_ID''')
+      rv.append(cur2.fetchall()[0[0]])
+
+
+      def sids_converter(o):
+            if isinstance(o, datetime.date):
+                  return str(o.year) + str("/") + str(o.month) + str("/") + str(o.day)
+
       json_data = []
-      json_data1 = []
-
-      row_headers = ['fl id', 'Division', 'GL Today', 'FL% ToDay' ]
-      row_headers1 = [ 'GL ToDate']
-      for row in rv:
-            json_data.append(dict(zip(row_headers,row)))
-      return json.dumps(json_data)
-
-            for row1 in rv1:
-                  json_data1.append(dict(zip(row_headers1, row1)))
-            return json.dumps(json_data1)
-
-
-
-
-
+      json_data.append(dict(zip(column_headers, rv)))
+      return json.dumps(json_data, default=sids_converter)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
 
-      #def sids_converter(o):
-            #if isinstance(o, datetime.date):
-                  #return str(o.year) + str("/") + str(o.month) + str("/") + str(o.day)
-
-      #for result in rv:
-            #json_data.append(dict(zip(row_headers, result)))
-      #return json.dumps(json_data, default=sids_converter)
 
 
 
